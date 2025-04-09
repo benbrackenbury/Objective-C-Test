fb4bcfc2a138beb5f5fd258b72b9764b90304d75
Ben Brackenbury <benbrack22@icloud.com>
2025-04-09 18:07:03 +0100

    Initial Commit

---
+++ ObjcTest1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ObjcTest1
+//
+//  Created by Ben Brackenbury on 4/9/25.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
